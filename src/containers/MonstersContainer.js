6147bfd43a63d1b2d36c1b86b9030814241732fa
klamb95 <klamb1995@gmail.com>
2021-05-14 11:05:29 +0100

    container working

---
+++ src/containers/MonstersContainer.js
@@ -0,0 +1,19 @@
+import {useState, useEffect} from 'react';
+
+
+const MonstersContainer = () => {
+
+
+return (
+    <div>
+        <p>ContainerTest</p>
+    </div>
+)
+
+};
+
+
+
+
+
+export default MonstersContainer; 
